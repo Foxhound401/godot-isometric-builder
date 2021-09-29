4b9b299ffba09a9be28de04bc2f92fd112b58c21
Phuc Phung <phungthienphuc18@gmail.com>
2021-09-29 23:32:13 +0700

    add EntityPlacer, allow the player to place the engine to the map

--- Systems/EntityPlacer.gd
+++ Systems/EntityPlacer.gd
@@ -1,16 +1,104 @@
 extends TileMap
 
+## Distance from the player when the mouse stops being able to interact.
+const MAXIMUM_WORK_DISTANCE := 275.0
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
+const POSITION_OFFSET := Vector2(0, 0)
+# const POSITION_OFFSET := Vector2(0, 25)
 
+var _blueprint: BlueprintEntity
 
-# Called when the node enters the scene tree for the first time.
+var _tracker: EntityTracker
+
+var _ground: TileMap
+
+var _player: KinematicBody2D
+
+onready var Library := {
+	"StirlingEngine": preload("res://Entities/Blueprints/StirlingEngineBlueprint.tscn").instance(),
+}
+
 func _ready() -> void:
-	pass # Replace with function body.
+	print("BLUEPRINT")
+	print(_blueprint)
+	Library[Library.StirlingEngine] = preload("res://Entities/Entities/StirlingEngineEntity.tscn")
+
+func _exit_tree() -> void:
+	Library.StirlingEngine.queue_free()
+
+func setup(tracker: EntityTracker, ground: TileMap, player: KinematicBody2D) -> void:
+	_tracker = tracker
+	_ground = ground
+	_player = player
+
+	for child in get_children():
+		if child is Entity:
+			var map_position := world_to_map(child.global_position)
+
+			_tracker.place_entity(child, map_position)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+func _unhandled_input(event: InputEvent) -> void:
+	var global_mouse_position := get_global_mouse_position()
+
+	var has_placeable_blueprint: bool = _blueprint and _blueprint.placeable
+
+	var is_close_to_player := (
+		global_mouse_position.distance_to(_player.global_position) < MAXIMUM_WORK_DISTANCE
+	)
+	
+	var cellv := world_to_map(global_mouse_position)
+
+	var cell_is_occupied := _tracker.is_cell_occupied(cellv)
+
+	var is_on_ground := _ground.get_cellv(cellv) == 0
+
+	if event.is_action_pressed("left_click"):
+		if has_placeable_blueprint:
+			if not cell_is_occupied and is_close_to_player and is_on_ground:
+				_place_entity(cellv)
+
+	elif event is InputEventMouseMotion:
+		if has_placeable_blueprint:
+			_move_blueprint_in_world(cellv)
+
+	elif event.is_action_pressed("drop") and _blueprint:
+		remove_child(_blueprint)
+		_blueprint = null
+
+	elif event.is_action_pressed("quickbar_1"):
+		if _blueprint:
+			remove_child(_blueprint)
+		_blueprint = Library.StirlingEngine
+		add_child(_blueprint)
+		_move_blueprint_in_world(cellv)
+
+
+func _move_blueprint_in_world(cellv: Vector2) -> void:
+	## check this because it has the POSITION_OFFSET
+	_blueprint.global_position = map_to_world(cellv) + POSITION_OFFSET
+
+	var is_close_to_player := (
+		get_global_mouse_position().distance_to(_player.global_position) < MAXIMUM_WORK_DISTANCE
+	)
+
+	print("GROUND CELL")
+	print(_ground.get_cellv(cellv))
+	var is_on_ground: bool = _ground.get_cellv(cellv) == 0
+	var cell_is_occupied := _tracker.is_cell_occupied(cellv)
+
+	if not cell_is_occupied and is_close_to_player and is_on_ground:
+		_blueprint.modulate = Color.white
+	else:
+		_blueprint.modulate = Color.red
+
+func _place_entity(cellv: Vector2) -> void:
+	var new_entity: Node2D = Library[_blueprint].instance()
+
+	add_child(new_entity)
+
+	new_entity.global_position = map_to_world(cellv) + POSITION_OFFSET
+
+	new_entity._setup(_blueprint)
+
+	_tracker.place_entity(new_entity, cellv)
--- Systems/Simulation.gd
+++ Systems/Simulation.gd
@@ -7,8 +7,11 @@ const INVISIBLE_BARRIER_ID := 2
 var _tracker := EntityTracker.new()
 
 onready var _ground := $GameWorld/GroundTiles
+onready var _entity_placer := $GameWorld/YSort/EntityPlacer
+onready var _player := $GameWorld/YSort/Player
 
 func _ready() -> void:
+	_entity_placer.setup(_tracker, _ground, _player)
 	var barriers: Array = _ground.get_used_cells_by_id(BARRIER_ID)
 
 	for barrier in barriers:
